5c5f7e08ffcce16017dd079f0625284145ef8442
Erfan Ghorbani <erfan2ghorbani@gmail.com>
2023-06-14 23:06:21 +0330

    throw json exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,17 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
+use Exception;
 
 class Handler extends ExceptionHandler
 {
@@ -26,5 +35,41 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception): JsonResponse|Response|RedirectResponse
+    {
+        if ($request->expectsJson()) {
+            return new JsonResponse(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest(route('login'));
+    }
+
+    public function render($request, Exception|Throwable $e)
+    {
+        if ($e instanceof MethodNotAllowedException) {
+            $message = $e->getMessage();
+
+            return new JsonResponse(['errors' => $message], 404);
+        }
+
+        if ($e instanceof ValidationException) {
+            $errors = $e->errors();
+
+            return new JsonResponse(['errors' => $errors], 422);
+        }
+
+        if ($e instanceof ModelNotFoundException) {
+
+            return new JsonResponse(['errors' => $e->getMessage()], 404);
+        }
+
+        if ($e instanceof QueryException) {
+
+            return new JsonResponse(['errors' => $e->getMessage()], 500);
+        }
+
+        return parent::render($request, $e);
     }
 }
